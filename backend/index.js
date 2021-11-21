b1e956fe58d5a9c298f531d79cf23d7e46509e53
Mateo Galić <mateogalic112@gmail.com>
2021-11-21 22:22:08 +0100

    simple http method handling

--- backend/index.js
+++ backend/index.js
@@ -1,11 +1,61 @@
+const fs = require("fs");
 const express = require("express");
 
 const app = express();
 
-app.get("/", (req, res) => {
+app.use(express.json());
+
+const tours = JSON.parse(
+  fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`)
+);
+
+app.get("/api/v1/tours", (req, res) => {
   res.status(200).json({
-    message: "Hello for the server side!",
+    status: "success",
+    results: tours.length,
+    data: {
+      tours,
+    },
   });
+});
+
+app.get("/api/v1/tours/:id", (req, res) => {
+  const id = parseInt(req.params.id);
+  const tour = tours.find((item) => item.id === id);
+
+  if (!tour) {
+    return res.status(404).json({
+      status: "fail",
+      message: "Invalid ID",
+    });
+  }
+
+  res.status(200).json({
+    status: "success",
+    data: {
+      tour,
+    },
+  });
+});
+
+app.post("/api/v1/tours", (req, res) => {
+  const id = tours.length + 1;
+  const newTour = Object.assign({ id }, req.body);
+
+  tours.push(newTour);
+
+  fs.writeFile(
+    `${__dirname}/dev-data/data/tours-simple.json`,
+    JSON.stringify(tours),
+    (err) => {
+      res.status(201).json({
+        status: "success",
+        data: {
+          tour: newTour,
+        },
+      });
+    }
+  );
 });
 
 const PORT = 5000;
