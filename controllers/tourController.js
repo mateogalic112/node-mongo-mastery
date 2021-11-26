63d1a240974280f83865c4c2867d75263087659c
Mateo Galić <mateogalic112@gmail.com>
2021-11-27 00:43:48 +0100

    Adding tour model

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -1,27 +1,12 @@
-const fs = require("fs");
-
-const tours = JSON.parse(
-  fs.readFileSync(`${__dirname}/../dev-data/data/tours-simple.json`)
-);
-
-exports.checkID = (req, res, next, val) => {
-  // req.params.id = val
-  if (val * 1 > tours.length) {
-    return res.status(404).json({
-      status: "fail",
-      message: "Invalid ID",
-    });
-  }
-  next();
-};
+const Tour = require('../models/tourModel');
 
 exports.checkBody = (req, res, next) => {
   const { name, price } = req.body;
 
   if (!name || !price) {
     return res.status(400).json({
-      status: "fail",
-      message: "Invalid name or price",
+      status: 'fail',
+      message: 'Invalid name or price',
     });
   }
   next();
@@ -29,58 +14,47 @@ exports.checkBody = (req, res, next) => {
 
 exports.getAllTours = (req, res) => {
   res.status(200).json({
-    status: "success",
-    results: tours.length,
+    status: 'success',
+    /* results: tours.length,
     data: {
       tours,
-    },
+    }, */
   });
 };
 
 exports.getTour = (req, res) => {
-  const id = parseInt(req.params.id);
-  const tour = tours.find((item) => item.id === id);
+  //const id = parseInt(req.params.id);
+  /* const tour = tours.find((item) => item.id === id);
 
   res.status(200).json({
-    status: "success",
+    status: 'success',
     data: {
       tour,
     },
-  });
+  }); */
 };
 
 exports.createTour = (req, res) => {
-  const id = tours.length + 1;
-  const newTour = Object.assign({ id }, req.body);
-
-  tours.push(newTour);
-
-  fs.writeFile(
-    `${__dirname}/dev-data/data/tours-simple.json`,
-    JSON.stringify(tours),
-    (err) => {
-      res.status(201).json({
-        status: "success",
-        data: {
-          tour: newTour,
-        },
-      });
-    }
-  );
+  res.status(201).json({
+    status: 'success',
+    /* data: {
+      tour: newTour,
+    }, */
+  });
 };
 
 exports.updateTour = (req, res) => {
   res.status(200).json({
-    status: "success",
+    status: 'success',
     data: {
-      tour: "Updated tour",
+      tour: 'Updated tour',
     },
   });
 };
 
 exports.deleteTour = (req, res) => {
   res.status(204).json({
-    status: "success",
+    status: 'success',
     data: null,
   });
 };
--- index.js
+++ index.js
@@ -1,8 +1,16 @@
+const mongoose = require('mongoose');
 const path = require('path');
+const app = require('./app');
 
 require('dotenv').config({ path: path.resolve(__dirname, './.env') });
 
-const app = require('./app');
+const DB = process.env.MONGO_URI;
+
+mongoose
+  .connect(DB, {
+    useNewUrlParser: true,
+  })
+  .then(() => console.log('DB Connection successful!'));
 
 const PORT = process.env.PORT || 5000;
 
