bb762266251c571e73429e4d7a7823efa4bd33b7
Mateo Galić <mateogalic112@gmail.com>
2021-11-27 09:35:30 +0100

    Mongoose data queries

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -1,46 +1,57 @@
 const Tour = require('../models/tourModel');
 
-exports.checkBody = (req, res, next) => {
-  const { name, price } = req.body;
+exports.getAllTours = async (req, res) => {
+  try {
+    const tours = await Tour.find({});
 
-  if (!name || !price) {
-    return res.status(400).json({
+    res.status(200).json({
+      status: 'success',
+      results: tours.length,
+      data: {
+        tours,
+      },
+    });
+  } catch (err) {
+    res.status(404).json({
       status: 'fail',
-      message: 'Invalid name or price',
+      message: 'Cannot get tours',
     });
   }
-  next();
 };
 
-exports.getAllTours = (req, res) => {
-  res.status(200).json({
-    status: 'success',
-    /* results: tours.length,
-    data: {
-      tours,
-    }, */
-  });
+exports.getTour = async (req, res) => {
+  try {
+    const tour = await Tour.findById(req.params.id);
+
+    res.status(200).json({
+      status: 'success',
+      data: {
+        tour,
+      },
+    });
+  } catch (err) {
+    res.status(404).json({
+      status: 'fail',
+      message: 'Cannot get tour',
+    });
+  }
 };
 
-exports.getTour = (req, res) => {
-  //const id = parseInt(req.params.id);
-  /* const tour = tours.find((item) => item.id === id);
-
-  res.status(200).json({
-    status: 'success',
-    data: {
-      tour,
-    },
-  }); */
-};
-
-exports.createTour = (req, res) => {
-  res.status(201).json({
-    status: 'success',
-    /* data: {
-      tour: newTour,
-    }, */
-  });
+exports.createTour = async (req, res) => {
+  try {
+    const newTour = await Tour.create(req.body);
+    res.status(201).json({
+      status: 'success',
+      data: {
+        tour: newTour,
+      },
+    });
+  } catch (err) {
+    res.status(400).json({
+      status: 'fail',
+      message: 'Invalid data',
+    });
+  }
 };
 
 exports.updateTour = (req, res) => {
