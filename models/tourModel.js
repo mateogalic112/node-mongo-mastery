910bece0e3f17f27e47114634be5c8e655000ada
Mateo Galić <mateogalic112@gmail.com>
2021-11-28 18:24:02 +0100

    Add custom validator

--- models/tourModel.js
+++ models/tourModel.js
@@ -41,7 +41,15 @@ const tourSchema = new mongoose.Schema(
       type: Number,
       required: [true, 'A tour must have a price'],
     },
-    priceDiscount: Number,
+    priceDiscount: {
+      type: Number,
+      validate: {
+        validator: function (value) {
+          return value < this.price;
+        },
+        message: 'Discount of ({VALUE}) cant be above regular price',
+      },
+    },
     summary: {
       type: String,
       trim: true,
