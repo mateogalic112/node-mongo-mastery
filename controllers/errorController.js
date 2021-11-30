64c54b374f2c660714c8911ac2b8760db42eb108
Mateo Galić <mateogalic112@gmail.com>
2021-11-30 19:05:44 +0100

    Handle mongo validation errors

--- controllers/errorController.js
+++ controllers/errorController.js
@@ -10,7 +10,13 @@ const handleDuplicateFieldsDB = (err) => {
     ', '
   )}`;
 
-  console.log(err);
+  return new AppError(message, 400);
+};
+
+const handleValidationErrorDB = (err) => {
+  const errors = Object.values(err.errors).map((item) => item.message);
+  const message = `Invalid input data. ${errors.join('. ')}`;
+
   return new AppError(message, 400);
 };
 
@@ -55,6 +61,11 @@ module.exports = (err, req, res, next) => {
     if (error.code === 11000) {
       error = handleDuplicateFieldsDB(error);
     }
+
+    if (error._message === 'Validation failed') {
+      error = handleValidationErrorDB(error);
+    }
+
     sendErrorProd(error, res);
   }
 };
