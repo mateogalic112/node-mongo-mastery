faba3f33e69f86318de814a97bf8c394ea508616
Mateo Galić <mateogalic112@gmail.com>
2021-12-04 12:14:48 +0100

    Limiting request body size

--- app.js
+++ app.js
@@ -23,7 +23,7 @@ const limiter = rateLimit({
 });
 app.use('/api', limiter);
 
-app.use(express.json());
+app.use(express.json({ limit: '10kb' }));
 app.use(express.static(`${__dirname}/public`));
 
 app.use('/api/v1/tours', tourRouter);
