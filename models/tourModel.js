db684515949c978e63e06e1157d3a1d40e9a7bd9
Mateo Galić <mateogalic112@gmail.com>
2021-12-06 20:03:01 +0100

    Add guides to tour

--- models/tourModel.js
+++ models/tourModel.js
@@ -98,6 +98,7 @@ const tourSchema = new mongoose.Schema(
         day: Number,
       },
     ],
+    guides: [{ type: mongoose.Schema.ObjectId, ref: 'User' }],
   },
   {
     toJSON: { virtuals: true },
@@ -115,6 +116,12 @@ tourSchema.pre('save', function (next) {
 
   next();
 });
+
+/* tourSchema.pre('save', async function (next) {
+  const guidesPromises = this.guides.map(async (id) => await User.findById(id));
+  this.guides = await Promise.all(guidesPromises);
+  next();
+}); */
 
 /* tourSchema.pre('save', function (next) {
   console.log('Will save document!');
