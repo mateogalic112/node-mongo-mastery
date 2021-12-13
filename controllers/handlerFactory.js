bcc2cf484d73d89dd1d4979c125a8f38c65d8843
Mateo Galić <mateogalic112@gmail.com>
2021-12-13 20:46:03 +0100

    Remove explain method from tour query

--- controllers/handlerFactory.js
+++ controllers/handlerFactory.js
@@ -80,7 +80,7 @@ exports.getAll = (Model) =>
       .limit()
       .paginate();
 
-    const doc = await features.query.explain();
+    const doc = await features.query;
 
     res.status(200).json({
       status: 'success',
