11b9626c0e89966ed10e598b2ab82e56514fd9c3
Mateo Galic <mateo.galic@asynclabs.co>
2021-12-31 16:59:25 +0100

    Add cookie parser

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const helmet = require('helmet');
 const sanitize = require('express-mongo-sanitize');
 const xss = require('xss-clean');
 const hpp = require('hpp');
+const cookieParser = require('cookie-parser');
 
 const AppError = require('./utils/appError');
 const globalErrorHandler = require('./controllers/errorController');
@@ -28,6 +29,7 @@ const limiter = rateLimit({
 app.use('/api', limiter);
 
 app.use(express.json({ limit: '10kb' }));
+app.use(cookieParser());
 
 app.use(sanitize());
 
